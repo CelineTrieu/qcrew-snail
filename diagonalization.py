0d384f49d096fb68b6454d2cd3c24e55013e8f41
fernandovldrs <fernandovldrs@gmail.com>
2021-08-27 23:45:15 +0800

    comparison script

--- diagonalization.py
+++ diagonalization.py
@@ -30,18 +30,25 @@ add_anharmonicity_plot(axes[1], evals)
 plt.show()
 
 # Create SNAIL2
+Ej = 14.851e9 * 3  # in Hz
 n = 3
-alpha = 0.45
-phi_ext = 0.45 * 2 * np.pi
-Lj = 17e-9
-freq = 4.6e9
+alpha = 0.29
+phi_ext = 0.39 * 2 * np.pi
+snail = SNAIL2(Ej, n, alpha, phi_ext)
 
-snail = SNAIL(n, alpha, phi_ext, Lj)
-ancilla = Ancilla(snail, freq)
+# Create ancillas for given shunt capacitance
+cap = 97.5e-15  # shunt capacitance in F
+ancilla = Ancilla2(snail, cap)
 
 # get qutip hamiltonian operator
 evals, evecs, H, a3, a4 = ancilla.calculate_spectrum()
 evals, evecs = clean_spectrum(evals, evecs)  # Remove weird states
+
+# Draw plots
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+add_spectrum_plot(ax, evals, evecs, ancilla.fock_trunc)
+plt.show()
 
 fig, axes = plt.subplots(2, 1, sharex=True)
 add_transition_energies_plot(axes[0], evals)
