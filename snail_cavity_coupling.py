37f2f16088a02e7d8fc5c6837bbea9ec2b304242
fernandovldrs <fernandovldrs@gmail.com>
2021-10-02 14:31:37 +0800

    added circuit class

--- snail_cavity_coupling.py
+++ snail_cavity_coupling.py
@@ -5,6 +5,8 @@ from qutip.qobj import Qobj
 from snail_solver.ancilla import Ancilla
 from snail_solver.helper_functions import *
 from snail_solver.snail_element import SNAIL
+from snail_solver.resonator import Resonator
+from snail_solver.circuit import Circuit
 from functools import reduce
 
 HFSS_project_name = "SQUID"
@@ -13,13 +15,17 @@ fock_trunc = 30
 # Create SNAIL (alpha, phi) = (0.35, 0.35), (0.4, 0.35)
 n = 3
 alpha = 0.33
-phi_ext = 0.390 * 2 * np.pi
+phi_ext = 0.410 * 2 * np.pi
 Lj = 25e-9  # HFSS lumped element inductance
-freq = np.array([5.104723e9, 7.465660e9])  # HFSS frequencies
-PHI_zpf = np.array([[0.62331647], [0.01441409]])
 snail = SNAIL.from_Lj(Lj, n, alpha, phi_ext)
 # snail = SNAIL.from_Ej(Ej, n, alpha, phi_ext)
+
+# Assemble circuit
+freq = np.array([5.104723e9, 7.465660e9])  # HFSS frequencies
+PHI_zpf = np.array([[0.62331647], [0.01441409]])
 ancilla = Ancilla(snail, freq[0], fock_trunc=fock_trunc)
+resonator = Resonator(freq[1], fock_trunc=fock_trunc)
+circuit = Circuit(ancilla, [resonator], PHI_zpf)
 
 # get eigenvectors of isolated SNAIL
 evals_0, evecs_0, H_0, taylor_coef = ancilla.calculate_spectrum()
@@ -48,95 +54,40 @@ def fock_on(d):
     return reduce(qutip.tensor, eigenvectors_list)
 
 
-def tensor_out(op, loc):
-    "Make operator <op> tensored with identities at locations other than <loc>"
-    op_list = [qutip.qeye(ancilla.fock_trunc) for i in range(n_modes)]
-    op_list[loc] = op
-    return reduce(qutip.tensor, op_list)
-
-
-def dot(ais, bis):
-    """
-    Dot product
-    """
-    return sum(ai * bi for ai, bi in zip(ais, bis))
-
-
-# intantiate qutip operators
-a = qutip.destroy(ancilla.fock_trunc)
-ad = a.dag()
-n = qutip.num(ancilla.fock_trunc)
-mode_fields = [tensor_out(a + ad, i) for i in range(n_modes)]
-mode_ns = [tensor_out(n, i) for i in range(n_modes)]
-
-# build hamiltonian
-cos_interiors = PHI_zpf[0, 0] * mode_fields[0] + PHI_zpf[1, 0] * mode_fields[1]
-Hl = dot(freq, mode_ns)
-Hnl = ancilla.Ej * ancilla.nl_potential(cos_interiors)
-H = Hl + Hnl
-print(ancilla.Ej)
-# diagonalize hamiltonian
-evals, evecs = H.eigenstates()
-
-
-def closest_state_to(s):
-    def distance(s2):
-        return (s.dag() * s2[1]).norm()
-
-    return max(zip(evals, evecs), key=distance)
+evals, evecs = circuit.eigenstates()
 
 
 fig, axes = plt.subplots(2, 1, sharex=True)
 # add_transition_energies_plot(axes[0], evals_0)
 # add_anharmonicity_plot(axes[1], evals_0)
 add_transition_energies_plot(
-    axes[0], [closest_state_to(eigenstate_on({0: i}))[0] for i in range(len(evecs_0))]
-)
-add_anharmonicity_plot(
-    axes[1], [closest_state_to(eigenstate_on({0: i}))[0] for i in range(len(evecs_0))]
-)
-add_transition_energies_plot(
     axes[0],
-    [closest_state_to(eigenstate_on({0: i, 1: 1}))[0] for i in range(len(evecs_0))],
+    [
+        closest_state_to(eigenstate_on({0: i}), evals, evecs)[0]
+        for i in range(len(evecs_0))
+    ],
 )
 add_anharmonicity_plot(
     axes[1],
-    [closest_state_to(eigenstate_on({0: i, 1: 1}))[0] for i in range(len(evecs_0))],
-)
-add_transition_energies_plot(
-    axes[0],
-    [closest_state_to(eigenstate_on({0: i, 1: 2}))[0] for i in range(len(evecs_0))],
-)
-add_anharmonicity_plot(
-    axes[1],
-    [closest_state_to(eigenstate_on({0: i, 1: 2}))[0] for i in range(len(evecs_0))],
-)
-add_transition_energies_plot(
-    axes[0],
-    [closest_state_to(eigenstate_on({0: i, 1: 3}))[0] for i in range(len(evecs_0))],
-)
-add_anharmonicity_plot(
-    axes[1],
-    [closest_state_to(eigenstate_on({0: i, 1: 3}))[0] for i in range(len(evecs_0))],
+    [
+        closest_state_to(eigenstate_on({0: i}), evals, evecs)[0]
+        for i in range(len(evecs_0))
+    ],
 )
 plt.show()
 
 fig, axes = plt.subplots(2, 1, sharex=True)
 add_transition_energies_plot(
-    axes[0], [closest_state_to(eigenstate_on({1: i}))[0] for i in range(len(evecs_0))]
-)
-add_anharmonicity_plot(
-    axes[1],
-    [1e6 * closest_state_to(eigenstate_on({1: i}))[0] for i in range(len(evecs_0))],
-)
-add_transition_energies_plot(
     axes[0],
-    [closest_state_to(eigenstate_on({0: 1, 1: i}))[0] for i in range(len(evecs_0))],
+    [
+        closest_state_to(eigenstate_on({1: i}), evals, evecs)[0]
+        for i in range(len(evecs_0))
+    ],
 )
 add_anharmonicity_plot(
     axes[1],
     [
-        1e6 * closest_state_to(eigenstate_on({0: 1, 1: i}))[0]
+        1e6 * closest_state_to(eigenstate_on({1: i}), evals, evecs)[0]
         for i in range(len(evecs_0))
     ],
 )
