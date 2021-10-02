2e702b1dc4263397e4705641b38aa7a16e5df4a0
fernandovldrs <fernandovldrs@gmail.com>
2021-10-02 11:38:10 +0800

    parameter adjustment

--- snail_cavity_coupling.py
+++ snail_cavity_coupling.py
@@ -8,21 +8,23 @@ from snail_solver.snail_element import SNAIL
 from functools import reduce
 
 HFSS_project_name = "SQUID"
+fock_trunc = 30
 
 # Create SNAIL (alpha, phi) = (0.35, 0.35), (0.4, 0.35)
 n = 3
-alpha = 0.32
-phi_ext = 0.34 * 2 * np.pi
-Lj = 11e-9  # HFSS lumped element inductance
-freq = np.array([5.19381e9, 7.46624e9])  # HFSS frequencies
-PHI_zpf = np.array([[0.41476267], [0.02612749]])
+alpha = 0.33
+phi_ext = 0.390 * 2 * np.pi
+Lj = 25e-9  # HFSS lumped element inductance
+freq = np.array([5.104723e9, 7.465660e9])  # HFSS frequencies
+PHI_zpf = np.array([[0.62331647], [0.01441409]])
 snail = SNAIL.from_Lj(Lj, n, alpha, phi_ext)
-ancilla = Ancilla(snail, freq[0])
+# snail = SNAIL.from_Ej(Ej, n, alpha, phi_ext)
+ancilla = Ancilla(snail, freq[0], fock_trunc=fock_trunc)
 
 # get eigenvectors of isolated SNAIL
 evals_0, evecs_0, H_0, taylor_coef = ancilla.calculate_spectrum()
 evals_0, evecs_0 = clean_spectrum(evals_0, evecs_0)
-
+print(taylor_coef[3])
 snail_mode = 0  # SNAIL mode number
 n_modes = 2  # total number of modes
 
@@ -72,7 +74,7 @@ cos_interiors = PHI_zpf[0, 0] * mode_fields[0] + PHI_zpf[1, 0] * mode_fields[1]
 Hl = dot(freq, mode_ns)
 Hnl = ancilla.Ej * ancilla.nl_potential(cos_interiors)
 H = Hl + Hnl
-
+print(ancilla.Ej)
 # diagonalize hamiltonian
 evals, evecs = H.eigenstates()
 
@@ -85,8 +87,8 @@ def closest_state_to(s):
 
 
 fig, axes = plt.subplots(2, 1, sharex=True)
-add_transition_energies_plot(axes[0], evals_0)
-add_anharmonicity_plot(axes[1], evals_0)
+# add_transition_energies_plot(axes[0], evals_0)
+# add_anharmonicity_plot(axes[1], evals_0)
 add_transition_energies_plot(
     axes[0], [closest_state_to(eigenstate_on({0: i}))[0] for i in range(len(evecs_0))]
 )
@@ -124,7 +126,8 @@ add_transition_energies_plot(
     axes[0], [closest_state_to(eigenstate_on({1: i}))[0] for i in range(len(evecs_0))]
 )
 add_anharmonicity_plot(
-    axes[1], [closest_state_to(eigenstate_on({1: i}))[0] for i in range(len(evecs_0))]
+    axes[1],
+    [1e6 * closest_state_to(eigenstate_on({1: i}))[0] for i in range(len(evecs_0))],
 )
 add_transition_energies_plot(
     axes[0],
@@ -132,6 +135,9 @@ add_transition_energies_plot(
 )
 add_anharmonicity_plot(
     axes[1],
-    [closest_state_to(eigenstate_on({0: 1, 1: i}))[0] for i in range(len(evecs_0))],
+    [
+        1e6 * closest_state_to(eigenstate_on({0: 1, 1: i}))[0]
+        for i in range(len(evecs_0))
+    ],
 )
 plt.show()
