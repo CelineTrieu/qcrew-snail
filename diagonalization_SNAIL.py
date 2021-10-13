549e6aa5936cdbb90392b15242cc451fc5e8f4af
fernandovldrs <fernandovldrs@gmail.com>
2021-10-14 05:34:02 +0800

    renaming and simplification

---
+++ diagonalization_SNAIL.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from snail_solver.ancilla import Ancilla
+from snail_solver.helper_functions import *
+from snail_solver.snail_element import SNAIL
+
+fock_trunc = 30
+
+# Create SNAIL
+n = 3
+alpha = 0.320
+phi_ext = 0.470 * 2 * np.pi
+Lj = 11.0e-9
+freq = 5.19381e9
+
+snail = SNAIL.from_Lj(Lj, n, alpha, phi_ext)
+ancilla = Ancilla(snail, freq, fock_trunc=fock_trunc)
+
+# get qutip hamiltonian operator
+evals, evecs = ancilla.calculate_spectrum()
+evals, evecs = clean_spectrum(evals, evecs)  # Remove weird states
+
+fig, axes = plt.subplots(2, 1, sharex=True)
+add_transition_energies_plot(axes[0], evals)
+add_anharmonicity_plot(axes[1], evals)
+plt.show()
