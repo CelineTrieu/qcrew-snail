97c019d5e696c708edcb6602ecde1389621ad295
fernandovldrs <fernandovldrs@gmail.com>
2021-10-14 05:32:13 +0800

    added get_epr_circuit_params function

--- snail_solver/epr_analysis.py
+++ snail_solver/epr_analysis.py
@@ -19,8 +19,31 @@ def epr_analysis(HFSS_project_path, HFSS_project_name, junction_info):
     eprh = epr.DistributedAnalysis(pinfo)
     eprh.do_EPR_analysis()  # Calculate participation ratios
 
-    # Instantiate QuantumAnalysis object to read EPR PHI_zpf results. The actual quantum
+    # Instantiate QuantumAnalysis object to read EPR results. The actual quantum
     # analysis will not take place.
     epra = epr.QuantumAnalysis(eprh.data_filename)
 
     return pinfo, eprh, epra
+
+
+def get_epr_circuit_params(pinfo, eprh, epra, variation):
+    """[summary]
+
+    Args:
+        pinfo ([type]): [description]
+        eprh ([type]): [description]
+        epra ([type]): [description]
+    """
+    epr_Lj = 1e-9 * float(
+        eprh.get_ansys_variables()
+        .loc[pinfo.junctions["j1"]["Lj_variable"]][variation]
+        .replace("nH", "")
+    )
+
+    epr_freqs = 1e9 * np.array(
+        eprh.get_ansys_frequencies_all().loc[str(variation)]["Freq. (GHz)"]
+    )
+
+    epr_phi_rzpf = epra.get_epr_base_matrices(str(variation), _renorm_pj=True)[4]
+
+    return epr_Lj, epr_freqs, epr_phi_rzpf
