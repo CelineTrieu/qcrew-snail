9d1f170b94420a32b7d6cd3a36088abed317f25c
fernandovldrs <fernandovldrs@gmail.com>
2021-11-25 09:42:34 +0800

    included possibily more than one resonator

--- ancilla_coupling_map.py
+++ ancilla_coupling_map.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
@@ -31,86 +30,76 @@ epr_Lj, epr_freqs, epr_phi_rzpf = get_epr_circuit_params(*_, variation)
 """
 
 # Define fixed circuit parameters
-fock_trunc = 10
+fock_trunc = 12
 n = 3
-Lj = 14e-9
-# freqs = [5.90273626e09, 6.31585224e09]  # , 7.12303432e+09]
-freqs = [4.76634030e09, 6.12085314e09]
-phi_rzpf = np.array([[0.43528079], [0.01620791]])
+Lj = 10e-9
+freqs = [5.04952809e09, 6.11913430e09, 6.93729448e09, 7.82853462e09]
+phi_rzpf = np.array([[0.36789528], [0.02069451], [0.02306255], [0.02154341]])
+ancilla_mode = np.argmax(phi_rzpf)
 
 # Sweep SNAIL parameters alpha and phi_ext
-cavity_kerr_list = []
-max_kerr_list = []
+
 alpha_list = np.arange(0.2, 0.5, 0.002)
-phi_ext_list = np.arange(0.2 * 2 * np.pi, 0.47 * 2 * np.pi, 0.002 * 2 * np.pi)
-for alpha in alpha_list:
-    for phi_ext in phi_ext_list:
+phi_ext_list = np.arange(0.2 * 2 * np.pi, 0.49 * 2 * np.pi, 0.002 * 2 * np.pi)
 
-        # assemble circuit
-        snail = SNAIL.from_Lj(Lj, {"n": 3, "alpha": alpha, "phi_ext": phi_ext})
-        ancilla = Ancilla(snail, freqs[np.argmax(phi_rzpf)], fock_trunc=fock_trunc)
-        circuit = Circuit(ancilla, freqs, phi_rzpf)
+# Picks one resonator from the list and simulate it with the ancilla
+for i in range(len(freqs)):
+    if ancilla_mode == i:
+        continue
+    cut_modes = [ancilla_mode, i]
+    cut_freqs = [freqs[index] for index in cut_modes]
+    cut_phis = np.array([[phi_rzpf[index][0]] for index in cut_modes])
 
-        # diagonalize coupled hamiltonian
-        evals, evecs = circuit.circuit_spectrum
+    cavity_kerr_list = []
+    for alpha in alpha_list:
+        for phi_ext in phi_ext_list:
 
-        cavity_evals = [
-            circuit.get_eigenstate({1: i})[0] for i in range(fock_trunc - 3)
-        ]
+            # assemble circuit
+            snail = SNAIL.from_Lj(Lj, {"n": 3, "alpha": alpha, "phi_ext": phi_ext})
+            ancilla = Ancilla(snail, freqs[ancilla_mode], fock_trunc=fock_trunc)
+            circuit = Circuit(ancilla, cut_freqs, cut_phis)
 
-        # Calculate optimization variables
-        relative_evals = np.real(cavity_evals - cavity_evals[0])
-        transit_energies = relative_evals[1:] - relative_evals[:-1]
-        anharm = transit_energies[1:] - transit_energies[:-1]
-        avg_kerr = np.average(anharm)
-        max_kerr = np.max(np.abs(anharm))
+            # diagonalize coupled hamiltonian
+            evals, evecs = circuit.circuit_spectrum
 
-        # Save for plotting
-        cavity_kerr_list.append(abs(avg_kerr))
-        max_kerr_list.append(max_kerr)
-        print(alpha, phi_ext, avg_kerr)
+            cavity_evals = [
+                circuit.get_eigenstate({1: i})[0] for i in range(fock_trunc - 3)
+            ]
 
-# Adjust and reshape values for colormesh plotting
-phi_ext_list /= 2 * np.pi  # plot in units of 2pi
-reshape_dim = (len(alpha_list), len(phi_ext_list))
-cavity_kerr_list = np.reshape(cavity_kerr_list, reshape_dim)
-max_kerr_list = np.reshape(max_kerr_list, reshape_dim)
-# meshgrid
-X, Y = np.meshgrid(phi_ext_list, alpha_list)
+            # Calculate optimization variables
+            relative_evals = np.real(cavity_evals - cavity_evals[0])
+            transit_energies = relative_evals[1:] - relative_evals[:-1]
+            anharm = transit_energies[1:] - transit_energies[:-1]
+            avg_kerr = np.average(anharm)
 
-# Plot
-fig, axes = plt.subplots(1, 2, sharey=True, sharex=True)
+            # Save for plotting
+            cavity_kerr_list.append(abs(avg_kerr))
 
-im1 = axes[0].pcolormesh(
-    phi_ext_list,
-    alpha_list,
-    cavity_kerr_list,
-    norm=colors.LogNorm(vmin=cavity_kerr_list.min(), vmax=cavity_kerr_list.max()),
-    shading="auto",
-    cmap="Oranges",
-)
-fig.colorbar(im1, ax=axes[0])  # , extend="max"
-axes[0].set_title("Average cavity Kerr")
-axes[0].set_ylabel("Josephson energy proportion alpha")
-axes[0].set_xlabel("External flux (per flux quanta)")
-# Plot contour
-CS = axes[0].contour(X, Y, cavity_kerr_list, [10, 100, 500, 1000])
-axes[0].clabel(CS, inline=True, fontsize=9)
+    # Adjust and reshape values for colormesh plotting
+    phi_ext_list /= 2 * np.pi  # plot in units of 2pi
+    reshape_dim = (len(alpha_list), len(phi_ext_list))
+    cavity_kerr_list = np.reshape(cavity_kerr_list, reshape_dim)
+    # meshgrid
+    X, Y = np.meshgrid(phi_ext_list, alpha_list)
 
+    # Plot
+    fig, axes = plt.subplots()
 
-im2 = axes[1].pcolormesh(
-    phi_ext_list,
-    alpha_list,
-    max_kerr_list,
-    norm=colors.LogNorm(vmin=max_kerr_list.min(), vmax=max_kerr_list.max()),
-    shading="auto",
-    cmap="Oranges",
-)
-fig.colorbar(im2, ax=axes[1])  # , extend="max"
-axes[1].set_title("Maximum absolute cavity Kerr")
-axes[1].set_xlabel("External flux (per flux quanta)")
-# Plot contour
-CS = axes[1].contour(X, Y, max_kerr_list, [10, 100, 500, 1000])
-axes[1].clabel(CS, inline=True, fontsize=9)
+    im1 = axes.pcolormesh(
+        phi_ext_list,
+        alpha_list,
+        cavity_kerr_list,
+        norm=colors.LogNorm(vmin=cavity_kerr_list.min(), vmax=cavity_kerr_list.max()),
+        shading="auto",
+        cmap="Oranges",
+    )
+    fig.colorbar(im1, ax=axes)  # , extend="max"
+    axes.set_title("Average cavity Kerr")
+    axes.set_ylabel("Josephson energy proportion alpha")
+    axes.set_xlabel("External flux (per flux quanta)")
+    # Plot contour
+    CS = axes.contour(X, Y, cavity_kerr_list, [10, 100, 500, 1000])
+    axes.clabel(CS, inline=True, fontsize=9)
+
 
 plt.show()
