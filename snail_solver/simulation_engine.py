b01c7b045fcc04465997fb0a2160bca23ff34bd8
fernandovldrs <fernandovldrs@gmail.com>
2023-07-03 11:14:55 +0800

    adding rotating frame to simulation (wip)

--- snail_solver/simulation_engine.py
+++ snail_solver/simulation_engine.py
@@ -1,4 +1,9 @@
+"""
+The simulation engine will use the fock basis of the reference circuit
+"""
+
 import qutip
+from snail_solver.helper_functions import *
 
 class SimulationEngine():
 
@@ -10,13 +15,36 @@ class SimulationEngine():
         self.initial_state = None
         self.pulses = []
 
+        self.rotating_frame_fs = None # list of frequencies
+        self.reference_params = None # parameters of the reference circuit
+
     def add_pulse(self, pulse):
         self.pulses.append(pulse)
         return
     
     @property
     def H(self):
-        return [self.circuit.hamiltonian] + [p.hamiltonian() for p in self.pulses]
+        drift_hamiltonian = self.circuit.hamiltonian - self.rotating_frame_matrix
+        print(self.circuit.hamiltonian)
+        print(drift_hamiltonian)
+        return [drift_hamiltonian] + [p.hamiltonian() for p in self.pulses]
+    
+    @property
+    def rotating_frame_matrix(self):
+        if self.rotating_frame_fs is None:
+            print("Rotating frame not defined")
+            return 0
+        n_modes = len(self.rotating_frame_fs)
+        fock_trunc = self.circuit.fock_trunc
+        frame_matrix = 0
+        for n in range(n_modes):
+            freq = self.rotating_frame_fs[n]
+            frame_matrix += freq*tensor_out(qutip.num(fock_trunc), n, fock_trunc, n_modes)
+        return frame_matrix
+    
+    # @property
+    # def reference_circuit(self):
+    #     return 
 
     def simulate(self, t_sim, timeskip = 1):
         self.results = qutip.mesolve(self.H, self.initial_state, t_sim, options=qutip.Options(nsteps=2000))
